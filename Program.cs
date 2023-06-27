32abcb424248966611b9af555cb2cea1cf6cf7cd
IlhamNajiii <127828076+IlhamNajiii@users.noreply.github.com>
2023-06-27 14:46:27 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace person
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            person person1 = new person("Nana", 19);
+            person1.InfoPerson();
+
+            Karyawan Karyawan1 = new Karyawan("Adel", 18);
+            Karyawan1.InfoPerson();
+            Karyawan1.InfoPerson("Mahasiswa");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace person
+{
+    internal class person
+    {
+        public string Name { get; set; }
+        public int Umur { get; set; }
+
+        public person(string name, int umur)
+        {
+            Name = name;
+            Umur = umur;
+        }
+
+        public void InfoPerson()
+        {
+            Console.WriteLine("\nNama saya {0}, dan umur saya {1}", Name, Umur);
+        }
+    }
+    class Karyawan : person
+    {
+        string idKaryawan { get; set; }
+        string subjek { get; set; }
+
+        public Karyawan(string name, int umur) : base(name, umur) { }
+        public void InfoPerson(string subjek)
+        {
+            this.subjek = subjek;
+            Console.WriteLine("Saya disini sebagai {0}", this.subjek);
+        }
+    }
+}
